34fb92797b9fa7144f70178f14711101454e9c6a
Hansikarn <Hansika.karn@gmail.com>
2024-02-09 22:49:54 +0530

    getElementById task 3

---
+++ Task 3/index.js
@@ -0,0 +1,9 @@
+const mainheading = document.getElementById("main-heading");
+mainheading.textContent = 'Fruit World';
+mainheading.style.color = 'orange';
+const header = document.getElementById("header");
+header.style.backgroundColor = 'green';
+header.style.borderBottom = 'solid orange';
+document.getElementById('basket-heading').style.color = 'green';
+const thanks = document.getElementById('thanks');
+thanks.innerHTML = '<p>Please visit us again</p>'
